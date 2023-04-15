959b61294b7f7495653f8096f406f765d7ce2fe8
Mateusz Ratajczak <80542171+Fikus192@users.noreply.github.com>
2023-04-15 20:22:42 +0200

    Poszerzono routing.php

--- php_projektBD_Ratajczak/routing.php
+++ php_projektBD_Ratajczak/routing.php
@@ -3,15 +3,20 @@
 use core\App;
 use core\Utils;
 
-App::getRouter()->setDefaultRoute('register'); // akcja/ścieżka domyślna
-App::getRouter()->setLoginRoute('loginShow'); // akcja/ścieżka na potrzeby logowania (przekierowanie, gdy nie ma dostępu)
+App::getRouter()->setDefaultRoute('searchCanoe'); // akcja/ścieżka domyślna
+App::getRouter()->setLoginRoute('login'); // akcja/ścieżka na potrzeby logowania (przekierowanie, gdy nie ma dostępu)
 
-Utils::addRoute('canoeList',    'CanoeListCtrl');
-Utils::addRoute('loginShow',     'LoginCtrl');
-Utils::addRoute('login',         'LoginCtrl');
+Utils::addRoute('homePage',      'HomePageCtrl');
+
+Utils::addRoute('login',         'LoginCtrl');       
 Utils::addRoute('logout',        'LoginCtrl',       ['user','admin']); 
 Utils::addRoute('register',      'RegisterCtrl');
-Utils::addRoute('canoeNew',     'CanoeEditCtrl',	['user','admin']);
-Utils::addRoute('canoeEdit',    'CanoeEditCtrl',	['user','admin']);
-Utils::addRoute('canoeSave',    'CanoeEditCtrl',	['user','admin']);
-Utils::addRoute('canoeDelete',  'CanoeEditCtrl',	['admin']);
+
+Utils::addRoute('searchCanoe',   'SearchCanoeCtrl');
+Utils::addRoute('search',        'SearchCanoeCtrl');
+Utils::addRoute('select',        'SearchCanoeCtrl');
+
+Utils::addRoute('canoeNew',      'CanoeEditCtrl',	['user','admin']);
+Utils::addRoute('canoeEdit',     'CanoeEditCtrl');
+Utils::addRoute('canoeSave',     'CanoeEditCtrl',	['user','admin']);
+Utils::addRoute('canoeDelete',   'CanoeEditCtrl',	['admin']);
